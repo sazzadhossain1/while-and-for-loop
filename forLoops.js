d68370caaa7c7fffd624ce52dd9c4096ca87a62d
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-05-28 01:53:35 +0600

    loop

--- forLoops.js
+++ forLoops.js
@@ -1,5 +1,25 @@
 const text = "I won't repeat my code";
 
-for (let i = 1; i < 6; i++) {
-  console.log(`${text} repeated in ${i} times`);
+for (let i = 1; i <= 6; i++) {
+  //   console.log(`${text} repeated in ${i} times`);
+}
+
+const string = "I am coding with loops";
+// console.log(string[0]);
+// console.log(string.length);
+
+for (let i = 0; i < string.length; i++) {
+  //   console.log(`${text}  ${string[i]} `);
+}
+
+const profile = ["sazzad", "hossain", 25, "js developer"];
+// console.log(profile[profile.length - 1]);
+// let i = 0;
+// while (i < profile.length) {
+//   console.log(profile[i]);
+//   i++;
+// }
+
+for (let i = 0; i < profile.length; i++) {
+  console.log(profile[i]);
 }

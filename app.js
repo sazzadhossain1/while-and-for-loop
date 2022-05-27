58394058103eb43b3ceac4d601a7f6d21eca0132
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-05-28 01:04:36 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const text = "I won't repeat my code";
+let number = 1;
+
+while (number < 6) {
+  console.log(`${text} repeated in ${number} times`);
+  number = number + 1;
+}
---
+++ forLoops.js
@@ -0,0 +1,5 @@
+const text = "I won't repeat my code";
+
+for (let i = 1; i < 6; i++) {
+  console.log(`${text} repeated in ${i} times`);
+}
